2600947af8b8bce2f4b50eb0bdb77e74c019dde3
AlvinCayo <cayoalvin991@gmail.com>
2026-04-18 16:47:07 -0400

    Estructura_Inicial_AURA

---
+++ frontend/constants/Colors.ts
@@ -0,0 +1,29 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
+
+export const AuraColors = {
+  primary: '#4E161F',
+  secondary: '#69151F',
+  accent: '#6F1F28',
+  black: '#000000',
+  gold: '#AB9173',
+  background: '#F2E9E2',
+  white: '#FFFFFF'
+};
